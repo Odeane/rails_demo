7ecf68572e0110887e2c1a765d31feff1cccac19
odeane_croney <odeane_croney@yahoo.com>
2020-06-28 00:26:09 +0200

    refactoring some code

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,7 +1,10 @@
+require 'rails_helper'
+
 RSpec.describe Article, type: :model do
     describe 'DB table' do
       it { is_expected.to have_db_column :id }
       it { is_expected.to have_db_column :title }
+      it { is_expected.to have_db_column :content }
     end
     
     describe 'Validations' do
